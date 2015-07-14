9cc5f5237824e063a837d15e38cede8987f0b389
Graham Watson <graham@phantomwatson.com>
2015-07-14 13:41:47 -0400

    Add login and logout methods

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -14,5 +14,35 @@ class UsersController extends AppController
     public function initialize()
     {
         parent::initialize();
+        $this->Auth->allow(['login', 'logout']);
+    }
+
+    public function login()
+    {
+        if ($this->request->is('post')) {
+            $user = $this->Auth->identify();
+            if ($user) {
+                $this->Auth->setUser($user);
+                if ($this->Auth->authenticationProvider()->needsPasswordRehash()) {
+                    $user = $this->Users->get($this->Auth->user('id'));
+                    $user->password = $this->request->data('password');
+                    $this->Users->save($user);
+                }
+                $this->AutoLogin->setCookie();
+                return $this->redirect($this->Auth->redirectUrl());
+            } else {
+                $this->Flash->error('Email or password is incorrect');
+            }
+        } else {
+            $this->request->data['auto_login'] = true;
+        }
+        $this->set(array(
+            'title_for_layout' => 'Log in'
+        ));
+    }
+
+    public function logout()
+    {
+        return $this->redirect($this->Auth->logout());
     }
 }
