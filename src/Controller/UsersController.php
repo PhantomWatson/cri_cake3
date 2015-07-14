4ba3e41947ca3b4924f80914e9d41be23697a6f1
Graham Watson <graham@phantomwatson.com>
2015-07-14 13:41:23 -0400

    Initial commit

---
+++ src/Controller/UsersController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+/**
+ * Users Controller
+ *
+ * @property \App\Model\Table\UsersTable $Users
+ */
+class UsersController extends AppController
+{
+
+    public function initialize()
+    {
+        parent::initialize();
+    }
+}
