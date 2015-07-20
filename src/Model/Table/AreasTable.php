577000ec274db1c6f0ffd4a206be3ca861eaaca4
Graham Watson <graham@phantomwatson.com>
2015-07-20 18:39:07 -0400

    Add space

--- src/Model/Table/AreasTable.php
+++ src/Model/Table/AreasTable.php
@@ -275,7 +275,7 @@ class AreasTable extends Table
             ->where(['Area.id' => $area_id])
             ->contain([
                 'Statistic' => function ($q) {
-                    return $q->where(['Statistic.stat_category_id' => range(1,17)]);
+                    return $q->where(['Statistic.stat_category_id' => range(1, 17)]);
                 },
                 'StatCategory'
             ])
