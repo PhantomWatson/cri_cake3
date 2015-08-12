b95b7af444bd79689bdd4417b4736788b172fec4
Graham Watson <graham@phantomwatson.com>
2015-08-12 19:09:01 -0400

    Add index()

---
+++ src/Controller/Admin/SurveysController.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Controller\Admin;
+
+use App\Controller\AppController;
+
+class SurveysController extends AppController
+{
+    public function index()
+    {
+        $communitiesTable = TableRegistry::get('Communities');
+        $clientCommunities = $this->Community->getClientCommunityList();
+        $clientCommunityIds = array_keys($clientCommunities);
+        $this->paginate['Community'] = [
+            'conditions' => ['id' => $clientCommunityIds],
+            'contain' => [
+                'OfficialSurvey' => [
+                    'fields' => ['id']
+                ],
+                'OrganizationSurvey' => [
+                    'fields' => ['id']
+                ]
+            ],
+            'fields' => ['id', 'name', 'score']
+        ];
+        $this->set([
+            'titleForLayout' => 'Surveys',
+            'communities' => $this->paginate('Community')
+        ]);
+    }
+}
