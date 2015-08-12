42cb496870ee33c95ed5651b933cf8f323e8d1d9
Graham Watson <graham@phantomwatson.com>
2015-08-12 15:57:25 -0400

    Add index()

---
+++ src/Controller/Client/PurchasesController.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Controller\Client;
+
+use App\Controller\AppController;
+
+class PurchasesController extends AppController
+{
+    public function index()
+    {
+        $communitiesTable = TableRegistry::get('Communities');
+        $clientId = $this->getClientId();
+        $communityId = $communitiesTable->getClientCommunityId($clientId);
+
+        $productsTable = TableRegistry::get('Products');
+        $products = $productsTable->find('all');
+        $purchases = $this->Purchases->getAllForCommunity($communityId); // just needed for debugging
+
+        foreach ($products as &$product) {
+            // Massage product data
+            $product->description = str_ireplace('PwR3', 'PWR<sup>3</sup>', $product->description);
+            $product->price = '$'.number_format($product->price, 0);
+            $product->status = $this->Product->getPurchaseStatus($communityId, $product->id, $clientId);
+        }
+
+        $community = $communitiesTable->get($communityId);
+        $this->set([
+            'titleForLayout' => 'Products Purchased for '.$community->name,
+            'purchases' => $purchases,
+            'products' => $products
+        ]);
+    }
+}
