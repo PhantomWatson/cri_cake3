480a93e5f5d7e19726f9c0a60ce4d85b14e0f6f1
Graham Watson <graham@phantomwatson.com>
2015-08-12 16:19:19 -0400

    Add refund()

--- src/Controller/Admin/PurchasesController.php
+++ src/Controller/Admin/PurchasesController.php
@@ -34,4 +34,42 @@ class PurchasesController extends AppController
             'purchases' => $this->paginate()
         ]);
     }
+
+    public function refund($purchaseId)
+    {
+        try {
+            $purchase = $this->Purchases->get($purchaseId);
+        } catch (RecordNotFoundException $e) {
+            $this->Flash->error("Payment record #$purchaseId not found.");
+            $this->redirect(['action' => 'index']);
+        }
+
+        if ($this->request->is('post')) {
+            // Bounce user back if the purchase was already refunded
+            if ($purchase->refunded) {
+                $timestamp = strtotime($purchase->refunded);
+                $date = date('F j, Y', $timestamp);
+                $usersTable = TableRegistry::get('Users');
+                $this->Purchase->Refunder->id = $purchase->refunder_id;
+                try {
+                    $user = $usersTable->get($purchase->refunder_id);
+                    $admin = $user->name;
+                } catch (RecordNotFoundException $e) {
+                    $admin = "(unknown user #$purchase->refunder_id)";
+                }
+                $this->Flash->error("That purchase record was already marked refunded on $date by $admin.");
+            } else {
+                // Record refund
+                $purchase->refunded = date('Y-m-d H:i:s');
+                $purchase->refunder_id = $this->Auth->user('id');
+                if ($this->Purchases->save($purchase)) {
+                    $this->Flash->success('Refund recorded.');
+                } else {
+                    $this->Flash->error('There was an error saving that refund record.');
+                }
+            }
+        }
+
+        $this->redirect(['action' => 'index']);
+    }
 }
