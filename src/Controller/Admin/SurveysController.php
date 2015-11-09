29e844d00736705f3d0f4fdbdf51a5304a268543
Graham Watson <graham@phantomwatson.com>
2015-11-09 15:49:02 -0500

    Add invite()

--- src/Controller/Admin/SurveysController.php
+++ src/Controller/Admin/SurveysController.php
@@ -32,10 +32,10 @@ class SurveysController extends AppController
     public function view($surveyId = null)
     {
         $survey = $this->Surveys->get($surveyId);
-        $surveyStatus = $this->Surveys->getStatus($survey->community_id, $survey->type);
+        $surveyStatus = $this->Surveys->getStatus($survey->communityId, $survey->type);
 
         $communitiesTable = TableRegistry::get('Communities');
-        $community = $communitiesTable->get($survey->community_id);
+        $community = $communitiesTable->get($survey->communityId);
 
         /* Determines if this survey is currently being auto-imported
          * (because the community is in an appropriate stage of the CRI process) */
@@ -49,13 +49,13 @@ class SurveysController extends AppController
         $this->set([
             'titleForLayout' => $community->name.' '.ucwords($survey->type).'s Survey',
             'isAdmin' => true,
-            'isOpen' => $this->Surveys->isOpen($survey->community_id, $survey->type),
+            'isOpen' => $this->Surveys->isOpen($survey->communityId, $survey->type),
             'surveyUrl' => $survey->sm_url,
             'invitedRespondentCount' => $surveyStatus['invited_respondent_count'],
             'uninvitedRespondentCount' => $surveyStatus['uninvited_respondent_count'],
             'percentInvitedResponded' => $surveyStatus['percent_invited_responded'],
             'responsesChecked' => $surveyStatus['responses_checked'],
-            'communityId' => $survey->community_id,
+            'communityId' => $survey->communityId,
             'surveyType' => $survey->type,
             'surveyId' => $surveyId,
             'invitations' => $respondentsTable->getInvitedList($surveyId),
@@ -64,5 +64,44 @@ class SurveysController extends AppController
             'isAutomaticallyImported' => $isAutomaticallyImported,
             'autoImportFrequency' => $autoImportFrequency
         ]);
+    }
+
+    public function invite($surveyId = null)
+    {
+        $survey = $this->Surveys->get($surveyId);
+        $communityId = $survey->community_id;
+        $respondentType = $survey->type;
+        $respondentsTable = TableRegistry::get('Respondents');
+        $approvedRespondents = $respondentsTable->getApprovedList($surveyId);
+        $unaddressedUnapprovedRespondents = $respondentsTable->getUnaddressedUnapprovedList($surveyId);
+        $allRespondents = array_merge($approvedRespondents, $unaddressedUnapprovedRespondents);
+
+        if ($this->request->is('post')) {
+            $this->processInvitations(compact(
+                'approvedRespondents',
+                'unaddressedUnapprovedRespondents',
+                'surveyId',
+                'communityId',
+                'respondentType',
+                'allRespondents'
+            ));
+        }
+
+        // Looks dumb, but this is because it's the parameter for client_invite(), which shares a view
+        $respondentTypePlural = $respondentType.'s';
+
+        $communitiesTable = TableRegistry::get('Communities');
+        $community = $communitiesTable->get($survey->community_id);
+        $titleForLayout = $community->name.': Invite Community '.$respondentTypePlural;
+
+        $this->set(compact(
+            'respondentTypePlural',
+            'approvedRespondents',
+            'unaddressedUnapprovedRespondents',
+            'allRespondents',
+            'titleForLayout',
+            'surveyId'
+        ));
+        $this->render('..'.DS.'..'.DS.'Client'.DS.'Surveys'.DS.'invite');
     }
 }
