d90ab016f7088ec6e0ce92b294a2bbb1e835dd60
Graham Watson <graham@phantomwatson.com>
2015-11-09 15:12:40 -0500

    Add view()

--- src/Controller/Admin/SurveysController.php
+++ src/Controller/Admin/SurveysController.php
@@ -28,4 +28,41 @@ class SurveysController extends AppController
             'communities' => $this->paginate('Community')
         ]);
     }
+
+    public function view($surveyId = null)
+    {
+        $survey = $this->Surveys->get($surveyId);
+        $surveyStatus = $this->Surveys->getStatus($survey->community_id, $survey->type);
+
+        $communitiesTable = TableRegistry::get('Communities');
+        $community = $communitiesTable->get($survey->community_id);
+
+        /* Determines if this survey is currently being auto-imported
+         * (because the community is in an appropriate stage of the CRI process) */
+        $officialSurveyAutoImported = $community->score >= 2 && $community->score < 3 && $survey->type == 'official';
+        $organizationSurveyAutoImported = $community->score >= 3 && $community->score < 4 && $survey->type == 'organization';
+        $isAutomaticallyImported = $officialSurveyAutoImported || $organizationSurveyAutoImported;
+
+        $autoImportFrequency = $isAutomaticallyImported ? $this->Surveys->getPerSurveyAutoImportFrequency() : '';
+
+        $respondentsTable = TableRegistry::get('Respondents');
+        $this->set([
+            'titleForLayout' => $community->name.' '.ucwords($survey->type).'s Survey',
+            'isAdmin' => true,
+            'isOpen' => $this->Surveys->isOpen($survey->community_id, $survey->type),
+            'surveyUrl' => $survey->sm_url,
+            'invitedRespondentCount' => $surveyStatus['invited_respondent_count'],
+            'uninvitedRespondentCount' => $surveyStatus['uninvited_respondent_count'],
+            'percentInvitedResponded' => $surveyStatus['percent_invited_responded'],
+            'responsesChecked' => $surveyStatus['responses_checked'],
+            'communityId' => $survey->community_id,
+            'surveyType' => $survey->type,
+            'surveyId' => $surveyId,
+            'invitations' => $respondentsTable->getInvitedList($surveyId),
+            'hasNewResponses' => $this->Surveys->newResponsesHaveBeenReceived($surveyId),
+            'hasUninvitedUnaddressed' => $this->Surveys->hasUnaddressedUnapprovedRespondents($surveyId),
+            'isAutomaticallyImported' => $isAutomaticallyImported,
+            'autoImportFrequency' => $autoImportFrequency
+        ]);
+    }
 }
