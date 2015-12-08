3ffbe74f3af26f5d6a2259f63af0dbeb4a51678f
Graham Watson <graham@phantomwatson.com>
2015-12-08 17:03:40 -0500

    CamelCase var names

--- src/Model/Behavior/SurveyMonkeyBehavior.php
+++ src/Model/Behavior/SurveyMonkeyBehavior.php
@@ -9,8 +9,8 @@ class SurveyMonkeyBehavior extends Behavior
     public function getSurveyMonkeyObject()
     {
         require_once(ROOT.DS.'vendor'.DS.'ascension'.DS.'php-surveymonkey'.DS.'src'.DS.'Ascension'.DS.'SurveyMonkey.php');
-        $api_key = Configure::read('survey_monkey_api_key');
-        $access_token = Configure::read('survey_monkey_api_access_token');
-        return new \Ascension\SurveyMonkey($api_key, $access_token);
+        $apiKey = Configure::read('survey_monkey_api_key');
+        $accessToken = Configure::read('survey_monkey_api_access_token');
+        return new \Ascension\SurveyMonkey($apiKey, $accessToken);
     }
 }
