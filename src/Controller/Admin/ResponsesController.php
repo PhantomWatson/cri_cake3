6171c0a18462e4f72b9b85cc7b42a3f3ee81a8b2
Graham Watson <graham@phantomwatson.com>
2015-12-18 18:35:02 -0500

    Fix pagination

--- src/Controller/Admin/ResponsesController.php
+++ src/Controller/Admin/ResponsesController.php
@@ -6,25 +6,6 @@ use Cake\ORM\TableRegistry;
 
 class ResponsesController extends AppController
 {
-    private function adminViewPagination($surveyId)
-    {
-        $this->paginate['Response'] = [
-            'conditions' => ['survey_id' => $surveyId],
-            'contain' => [
-                'Respondent' => [
-                    'fields' => ['id', 'email', 'name', 'approved']
-                ]
-            ],
-            'order' => ['response_date' => 'DESC']
-        ];
-        $count = $this->Responses->find('all')
-            ->where(['survey_id' => $surveyId])
-            ->count();
-        if ($count) {
-            $this->paginate['Response']['limit'] = $count;
-        }
-        $this->cookieSort('AdminResponsesView');
-    }
 
     public function view($surveyId = null)
     {
@@ -47,8 +28,23 @@ class ResponsesController extends AppController
 
         $totalAlignment = 0;
         $this->adminViewPagination($surveyId);
-        $this->paginate['contain'] = ['Respondents'];
-        $responses = $this->paginate();
+        $count = $this->Responses->find('all')
+            ->where(['Responses.survey_id' => $surveyId])
+            ->count();
+        $query = $this->Responses
+            ->find('all')
+            ->where(['Responses.survey_id' => $surveyId])
+            ->contain([
+                'Respondents' => function ($q) {
+                    return $q->select(['id', 'email', 'name', 'approved']);
+                }
+            ])
+            ->order(['Responses.response_date' => 'DESC']);
+        if ($count) {
+            $query->limit($count);
+        }
+        $responses = $this->paginate($query);
+        $this->cookieSort('AdminResponsesView');
 
         // Only return the most recent response for each respondent
         $responsesReturned = [];
