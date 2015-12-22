db6cbb778f76373ee6e9d25e19574eb97a6868aa
Graham Watson <graham@phantomwatson.com>
2015-12-22 18:07:43 -0500

    Initial commit

---
+++ tests/TestCase/Controller/PagesControllerTest.php
@@ -0,0 +1,61 @@
+<?php
+namespace App\Test\TestCase\Controller;
+
+use App\Controller\PagesController;
+use Cake\TestSuite\IntegrationTestCase;
+
+/**
+ * App\Controller\PagesController Test Case
+ */
+class PagesControllerTest extends IntegrationTestCase
+{
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [];
+
+    public function testHome()
+    {
+        $this->get('/');
+        $this->assertResponseOk();
+    }
+
+    public function testGlossary()
+    {
+        $this->get('/glossary');
+        $this->assertResponseOk();
+    }
+
+    public function testFaqCommunity()
+    {
+        $this->get('/communityFAQ');
+        $this->assertResponseOk();
+    }
+
+    public function testFaqConsultants()
+    {
+        $this->get('/consultantFAQ');
+        $this->assertResponseOk();
+    }
+
+    public function testFasttrack()
+    {
+        $this->get('/fasttrack');
+        $this->assertResponseOk();
+    }
+
+    public function testCredits()
+    {
+        $this->get('/credits');
+        $this->assertResponseOk();
+    }
+
+    public function testEnroll()
+    {
+        $this->get('/enroll');
+        $this->assertResponseCode(302);
+    }
+}
