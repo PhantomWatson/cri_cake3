d0aecd1abdfaf8d678660355f7497a487079f366
Graham Watson <graham@phantomwatson.com>
2016-01-25 17:32:08 -0500

    Remove tutorial comments

--- config/routes.php
+++ config/routes.php
@@ -21,24 +21,6 @@
 use Cake\Core\Plugin;
 use Cake\Routing\Router;
 
-/**
- * The default class to use for all routes
- *
- * The following route classes are supplied with CakePHP and are appropriate
- * to set as the default:
- *
- * - Route
- * - InflectedRoute
- * - DashedRoute
- *
- * If no call is made to `Router::defaultRouteClass`, the class used is
- * `Route` (`Cake\Routing\Route\Route`)
- *
- * Note that `Route` does not do any inflections on URLs which will result in
- * inconsistently cased URLs when used with `:plugin`, `:controller` and
- * `:action` markers.
- *
- */
 Router::defaultRouteClass('DashedRoute');
 
 Router::prefix('admin', function ($routes) {
@@ -81,27 +63,7 @@ Router::scope('/', function ($routes) {
     $routes->connect('/surveys/get_qna_ids/*',             ['controller' => 'Surveys', 'action' => 'getQnaIds']);
     $routes->connect('/surveys/cron_import',               ['controller' => 'Surveys', 'action' => 'cronImport']);
 
-    /**
-     * Connect catchall routes for all controllers.
-     *
-     * Using the argument `InflectedRoute`, the `fallbacks` method is a shortcut for
-     *    `$routes->connect('/:controller', ['action' => 'index'], ['routeClass' => 'InflectedRoute']);`
-     *    `$routes->connect('/:controller/:action/*', [], ['routeClass' => 'InflectedRoute']);`
-     *
-     * Any route class can be used with this method, such as:
-     * - DashedRoute
-     * - InflectedRoute
-     * - Route
-     * - Or your own route class
-     *
-     * You can remove these routes once you've connected the
-     * routes you want in your application.
-     */
     $routes->fallbacks('DashedRoute');
 });
 
-/**
- * Load all plugin routes.  See the Plugin documentation on
- * how to customize the loading of plugin routes.
- */
 Plugin::routes();
