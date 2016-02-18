4e35f4fc53b491786b23b9b1d8c61981cc026d2f
Graham Watson <graham@phantomwatson.com>
2016-02-18 16:48:30 -0500

    Define format for input

--- config/bootstrap_form.php
+++ config/bootstrap_form.php
@@ -5,6 +5,7 @@ return [
 	'inputContainerError' => '<div class="form-group has-error has-feedback {{type}}{{required}}">{{content}}{{error}}</div>',
 	'select' => '<select class="form-control" name="{{name}}"{{attrs}}>{{content}}</select>',
 	'selectMultiple' => '<select class="form-control" name="{{name}}[]" multiple="multiple"{{attrs}}>{{content}}</select>',
+    'input' => '<input class="form-control" type="{{type}}" name="{{name}}"{{attrs}}>',
 
 	/* defaults
 	'button' => '<button{{attrs}}>{{text}}</button>',
