f6f2c7b7d79ef0dbfa54923cfe302d5d09d5987c
Graham Watson <graham@phantomwatson.com>
2016-12-13 16:14:12 -0500

    Avoid defining function in a loop

--- webroot/js/admin.js
+++ webroot/js/admin.js
@@ -976,14 +976,15 @@ var presentationsForm = {
 var adminGuide = {
     init: function () {
         var sections = $('section.admin-guide');
+        var clickFunction = function () {
+            var section = $(this).parents('section');
+            adminGuide.toggleSection(section);
+        };
         for (var i = 0; i < sections.length; i++) {
             var section = $(sections[i]);
             var header = section.find('h2');
             var button = $('<button class="btn btn-default btn-block"></button>');
-            button.click(function () {
-                var section = $(this).parents('section');
-                adminGuide.toggleSection(section);
-            });
+            button.click(clickFunction);
             header.wrapInner(button);
         }
     },
