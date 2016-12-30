b09b4499ece69bf1a2cc306dcfc294e05fbb760b
Graham Watson <graham@phantomwatson.com>
2016-12-30 16:35:34 -0500

    Refactor

--- src/SurveyMonkey/SurveyMonkey.php
+++ src/SurveyMonkey/SurveyMonkey.php
@@ -23,24 +23,12 @@ class SurveyMonkey
      */
     public function __construct()
     {
-        $this->api = $this->getSurveyMonkeyObject();
-    }
-
-    /**
-     * Returns a SurveyMonkey object
-     *
-     * Uses liogi/surveymonkey-api-v3
-     *
-     * @return SurveyMonkey
-     */
-    public function getSurveyMonkeyObject()
-    {
         $path = ROOT . DS . 'vendor' . DS . 'liogi' . DS . 'surveymonkey-api-v3' . DS . 'lib';
         require_once $path . DS . 'SurveyMonkey.php';
         $apiKey = Configure::read('survey_monkey_api_key');
         $accessToken = Configure::read('survey_monkey_api_access_token');
 
-        return new \SurveyMonkey($apiKey, $accessToken);
+        $this->api = new \SurveyMonkey($apiKey, $accessToken);
     }
 
     /**
