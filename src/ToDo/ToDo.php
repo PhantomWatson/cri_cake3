847765a4ade1a0a3001f279dc83802abb816c9de
Graham Watson <graham@phantomwatson.com>
2017-02-09 18:26:21 -0500

    Add "ready to advance to step two" item

--- src/ToDo/ToDo.php
+++ src/ToDo/ToDo.php
@@ -5,11 +5,26 @@ use App\Model\Table\ProductsTable;
 use Cake\ORM\TableRegistry;
 use Cake\Routing\Router;
 
+/**
+ * Handles the logic for the Admin To-Do page
+ *
+ * Each ready...() and waiting...() method assumes that none of the previous checks called before
+ * it in getToDo() returned positive. In other words, these checks are meant to work in a chain,
+ * but not in isolation.
+ *
+ * Class ToDo
+ * @package App\ToDo
+ */
 class ToDo
 {
     public $communitiesTable;
     public $productsTable;
 
+    /**
+     * ToDo constructor
+     *
+     * @return ToDo
+     */
     public function __construct()
     {
         $this->communitiesTable = TableRegistry::get('Communities');
@@ -32,6 +47,7 @@ class ToDo
                 'action' => 'clients',
                 $communityId
             ]);
+
             return [
                 'class' => 'ready',
                 'msg' => 'Ready to <a href="' . $url . '">assign a client</a>'
@@ -40,9 +56,24 @@ class ToDo
 
         if ($this->waitingForOfficialsSurveyPurchase($communityId)) {
             $product = $this->productsTable->get(ProductsTable::OFFICIALS_SURVEY);
+
             return [
                 'class' => 'waiting',
                 'msg' => 'Waiting for client to purchase ' . $product->description
+            ];
+        }
+
+        if ($this->readyToAdvanceToStepTwo($communityId)) {
+            $url = Router::url([
+                'prefix' => 'admin',
+                'controller' => 'Communities',
+                'action' => 'progress',
+                $communityId
+            ]);
+
+            return [
+                'class' => 'ready',
+                'msg' => 'Ready to <a href="' . $url . '">advance to Step Two</a>'
             ];
         }
 
@@ -76,5 +107,18 @@ class ToDo
         $productId = ProductsTable::OFFICIALS_SURVEY;
 
         return ! $this->productsTable->isPurchased($communityId, $productId);
+    }
+
+    /**
+     * Returns whether or not the community is ready to advance to Step Two
+     *
+     * @param int $communityId Community ID
+     * @return bool
+     */
+    private function readyToAdvanceToStepTwo($communityId)
+    {
+        $community = $this->communitiesTable->get($communityId);
+
+        return $community->score < 2;
     }
 }
