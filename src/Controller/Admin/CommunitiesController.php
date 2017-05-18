e4e4c8cc172f782879279e4374d32d5fe0ea05c9
Graham Watson <graham@phantomwatson.com>
2017-05-18 18:37:59 -0400

    Remove $communityId from activity log event
    Was creating an error, since $communityId no longer passed the existsIn test.

--- src/Controller/Admin/CommunitiesController.php
+++ src/Controller/Admin/CommunitiesController.php
@@ -360,7 +360,6 @@ class CommunitiesController extends AppController
 
             // Dispatch event
             $event = new Event('Model.Community.afterDelete', $this, ['meta' => [
-                'communityId' => $communityId,
                 'communityName' => $community->name
             ]]);
             $this->eventManager()->dispatch($event);
