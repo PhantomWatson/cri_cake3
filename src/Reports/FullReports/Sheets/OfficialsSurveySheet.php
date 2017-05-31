9fa8bee5a3a6757e5641ad69f488d657fe4796e0
Graham Watson <graham@phantomwatson.com>
2017-05-31 16:28:08 -0400

    Remove unnecessary subclassing

--- src/Reports/FullReports/Sheets/OfficialsSurveySheet.php
+++ src/Reports/FullReports/Sheets/OfficialsSurveySheet.php
@@ -4,7 +4,7 @@ namespace App\Reports\FullReports\Sheets;
 use App\Reports\Spreadsheet;
 use Cake\ORM\TableRegistry;
 
-class OfficialsSurveySheet extends Spreadsheet
+class OfficialsSurveySheet
 {
     /**
      * Adds an Officials Survey sheet to the provided workbook and returns the workbook
