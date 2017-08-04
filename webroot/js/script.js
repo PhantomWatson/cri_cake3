0f0f57647f32350660e5e7b1587156f8a4fec5e7
Graham Watson <graham@phantomwatson.com>
2017-08-04 17:08:34 -0400

    Refactor flashMessage

--- webroot/js/script.js
+++ webroot/js/script.js
@@ -31,26 +31,31 @@ var flashMessage = {
             messages.slideDown(500);
         }
     },
-    insert: function (message, classname) {
-        var bootstrap_class = 'alert-info';
-        if (classname === 'error') {
-            bootstrap_class = 'alert-danger';
-        } else if (classname === 'success') {
-            bootstrap_class = 'alert-success';
+    insert: function (message, className) {
+        var alert = $('<div class="alert alert-dismissible" role="alert"></div>');
+        switch (className) {
+            case 'error':
+                alert.addClass('alert-danger');
+                break;
+            case 'success':
+                alert.addClass('alert-success');
+                break;
+            default:
+                alert.addClass('alert-info');
         }
-        
-        var alert = $('<div class="alert alert-dismissible '+bootstrap_class+'" role="alert"></div>');
-        alert.append('<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span></button>');
-        alert.append(message);
-        
+        var button = $('<button type="button" class="close" data-dismiss="alert"></button>')
+            .append('<span aria-hidden="true">&times;</span>');
+        alert.append(button).append(message);
+
         var container = $('#flash_messages_bootstrap');
         if (container.is(':visible')) {
             alert.hide();
             container.append(alert);
             alert.slideDown();
-        } else {
-            container.append(alert);
-            container.slideDown();
+            return;
         }
+
+        container.append(alert);
+        container.slideDown();
     } 
 };
