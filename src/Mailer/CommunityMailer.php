a62a521ca0b87ddcb53d32eeac3f44f3dd38578a
Graham Watson <graham@phantomwatson.com>
2017-08-29 12:58:13 -0400

    Create CommunityMailer::communityPromoted()

---
+++ src/Mailer/CommunityMailer.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Mailer;
+
+use Cake\Mailer\Email;
+use Cake\Mailer\Mailer;
+use Cake\Routing\Router;
+
+class CommunityMailer extends Mailer
+{
+    /**
+     * Defines an email informing a user that their community has been advanced to the next step
+     *
+     * @param array $user User data
+     * @param array $community Community data
+     * @param int $toStep The step number the community has just been promoted to
+     * @return Email
+     */
+    public function communityPromoted($user, $community, $toStep)
+    {
+        return $this
+            ->setTo($user['email'])
+            ->setSubject('Community Readiness Initiative - Your community has advanced to Step ' . $toStep)
+            ->setViewVars([
+                'communityName' => $community['name'],
+                'homeUrl' => Router::url('/', true),
+                'toStep' => $toStep,
+                'userName' => $user['name']
+            ])
+            ->setTemplate('community_promoted');
+    }
+}
---
+++ src/Template/Email/text/community_promoted.ctp
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var string $homeUrl
+ * @var string $userName
+ * @var string $communityName
+ * @var string $toStep
+ */
+?>
+
+<?= $userName ?>,
+
+<?= $communityName ?> has been advanced to Step <?= $toStep ?> of the Community Readiness Initiative. For more information about this phase of the CRI process, please go to <?= $homeUrl ?> to log in and visit the CRI Client Home page.
+
+If you have any questions, please email cri@bsu.edu.
