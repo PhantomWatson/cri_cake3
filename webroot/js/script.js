bf9aa8d5f52d0976d025076dece6c2fd18454801
Graham Watson <graham@phantomwatson.com>
2017-09-19 19:16:34 -0400

    Delete code made redundant by new flash component
    The Data Center plugin's flash component, that is.

--- webroot/js/script.js
+++ webroot/js/script.js
@@ -23,39 +23,3 @@ var sidebar = {
         });
     }
 };
-
-var flashMessage = {
-    init: function () {
-        var messages = $('#flash_messages_bootstrap');
-        if (! messages.is(':visible')) {
-            messages.slideDown(500);
-        }
-    },
-    insert: function (message, className) {
-        var alert = $('<div class="alert alert-dismissible" role="alert"></div>');
-        switch (className) {
-            case 'error':
-                alert.addClass('alert-danger');
-                break;
-            case 'success':
-                alert.addClass('alert-success');
-                break;
-            default:
-                alert.addClass('alert-info');
-        }
-        var button = $('<button type="button" class="close" data-dismiss="alert"></button>')
-            .append('<span aria-hidden="true">&times;</span>');
-        alert.append(button).append(message);
-
-        var container = $('#flash_messages_bootstrap');
-        if (container.is(':visible')) {
-            alert.hide();
-            container.append(alert);
-            alert.slideDown();
-            return;
-        }
-
-        container.append(alert);
-        container.slideDown();
-    } 
-};
