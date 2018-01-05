0af0a423a776283af5258bc58fd5f2f73e835435
Graham Watson <graham@phantomwatson.com>
2018-01-05 15:39:09 -0500

    Add preview methods for various emails

--- src/Mailer/Preview/AdminTaskEmailPreview.php
+++ src/Mailer/Preview/AdminTaskEmailPreview.php
@@ -2,7 +2,9 @@
 namespace App\Mailer\Preview;
 
 use App\Mailer\AdminTaskMailer;
+use App\Model\Table\ProductsTable;
 use Cake\Mailer\Email;
+use Cake\ORM\TableRegistry;
 use DebugKit\Mailer\MailPreview;
 
 class AdminTaskEmailPreview extends MailPreview
@@ -19,9 +21,7 @@ class AdminTaskEmailPreview extends MailPreview
                 'name' => 'Recipient Name',
                 'email' => 'recipient@example.com'
             ],
-            'community' => [
-                'name' => 'Community Name'
-            ],
+            'community' => ['name' => 'Community Name'],
             'surveyType' => 'official'
         ];
 
@@ -29,5 +29,177 @@ class AdminTaskEmailPreview extends MailPreview
         $mailer = $this->getMailer('AdminTask');
 
         return $mailer->deliverMandatoryPresentation($data);
+    }
+
+    /**
+     * Preview method for CommunityMailer::deliverOptionalPresentation()
+     *
+     * @return Email
+     */
+    public function deliverOptionalPresentation()
+    {
+        $data = [
+            'user' => [
+                'name' => 'Recipient Name',
+                'email' => 'recipient@example.com'
+            ],
+            'community' => ['name' => 'Community Name'],
+            'productId' => ProductsTable::OFFICIALS_SUMMIT
+        ];
+
+        /** @var AdminTaskMailer $mailer */
+        $mailer = $this->getMailer('AdminTask');
+
+        return $mailer->deliverOptionalPresentation($data);
+    }
+
+    /**
+     * Preview method for CommunityMailer::createSurvey()
+     *
+     * @return Email
+     */
+    public function createSurvey()
+    {
+        $data = [
+            'user' => [
+                'name' => 'Recipient Name',
+                'email' => 'recipient@example.com'
+            ],
+            'community' => [
+                'name' => 'Community Name',
+                'slug' => 'community-name'
+            ],
+            'newSurveyType' => 'official',
+            'toStep' => 2
+        ];
+
+        /** @var AdminTaskMailer $mailer */
+        $mailer = $this->getMailer('AdminTask');
+
+        return $mailer->createSurvey($data);
+    }
+
+    /**
+     * Preview method for CommunityMailer::createSurveyNewCommunity()
+     *
+     * @return Email
+     */
+    public function createSurveyNewCommunity()
+    {
+        $data = [
+            'user' => [
+                'name' => 'Recipient Name',
+                'email' => 'recipient@example.com'
+            ],
+            'community' => [
+                'name' => 'Community Name',
+                'slug' => 'community-name'
+            ]
+        ];
+
+        /** @var AdminTaskMailer $mailer */
+        $mailer = $this->getMailer('AdminTask');
+
+        return $mailer->createSurveyNewCommunity($data);
+    }
+
+    /**
+     * Preview method for CommunityMailer::schedulePresentation()
+     *
+     * @return Email
+     */
+    public function schedulePresentation()
+    {
+        $communitiesTable = TableRegistry::get('Communities');
+        $data = [
+            'user' => [
+                'name' => 'Recipient Name',
+                'email' => 'recipient@example.com'
+            ],
+            'community' => [
+                'id' => $communitiesTable->find()->first()->id,
+                'name' => 'Community Name'
+            ],
+            'deliverableId' => ProductsTable::OFFICIALS_SUMMIT
+        ];
+
+        /** @var AdminTaskMailer $mailer */
+        $mailer = $this->getMailer('AdminTask');
+
+        return $mailer->schedulePresentation($data);
+    }
+
+    /**
+     * Preview method for CommunityMailer::deliverPolicyDev()
+     *
+     * @return Email
+     */
+    public function deliverPolicyDev()
+    {
+        $communitiesTable = TableRegistry::get('Communities');
+        $data = [
+            'user' => [
+                'name' => 'Recipient Name',
+                'email' => 'recipient@example.com'
+            ],
+            'community' => [
+                'id' => $communitiesTable->find()->matching('Clients')->first()->id,
+                'name' => 'Community Name'
+            ]
+        ];
+
+        /** @var AdminTaskMailer $mailer */
+        $mailer = $this->getMailer('AdminTask');
+
+        return $mailer->deliverPolicyDev($data);
+    }
+
+    /**
+     * Preview method for CommunityMailer::assignClient()
+     *
+     * @return Email
+     */
+    public function assignClient()
+    {
+        $communitiesTable = TableRegistry::get('Communities');
+        $data = [
+            'user' => [
+                'name' => 'Recipient Name',
+                'email' => 'recipient@example.com'
+            ],
+            'community' => [
+                'id' => $communitiesTable->find()->matching('Clients')->first()->id,
+                'name' => 'Community Name'
+            ]
+        ];
+
+        /** @var AdminTaskMailer $mailer */
+        $mailer = $this->getMailer('AdminTask');
+
+        return $mailer->assignClient($data);
+    }
+
+    /**
+     * Preview method for CommunityMailer::activateSurvey()
+     *
+     * @return Email
+     */
+    public function activateSurvey()
+    {
+        $data = [
+            'user' => [
+                'name' => 'Recipient Name',
+                'email' => 'recipient@example.com'
+            ],
+            'community' => [
+                'name' => 'Community Name'
+            ],
+            'surveyType' => 'organization'
+        ];
+
+        /** @var AdminTaskMailer $mailer */
+        $mailer = $this->getMailer('AdminTask');
+
+        return $mailer->activateSurvey($data);
     }
 }
