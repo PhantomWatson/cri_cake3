a64e54a3ed2dc3228ba37e45a51b8f721c3a6fd4
Graham Watson <graham@phantomwatson.com>
2018-01-09 11:57:11 -0500

    Add testSendOptPresEmail()

--- tests/TestCase/Event/EmailListenerTest.php
+++ tests/TestCase/Event/EmailListenerTest.php
@@ -2,6 +2,7 @@
 namespace App\Test\TestCase\Event;
 
 use App\Event\EmailListener;
+use App\Model\Table\ProductsTable;
 use App\Test\TestCase\ApplicationTest;
 use Cake\Event\Event;
 
@@ -36,5 +37,23 @@ class EmailListenerTest extends ApplicationTest
         $meta = ['communityId' => 1];
         $listener->sendDeliverMandatoryPresentationEmail($event, $meta);
         $this->assertAdminTaskEmailEnqueued('deliverMandatoryPresentation');
+    }
+
+    /**
+     * Tests EmailListener::sendDeliverOptPresentationEmail()
+     *
+     * @return void
+     * @throws \Exception
+     */
+    public function testSendOptPresEmail()
+    {
+        $listener = new EmailListener();
+        $event = new Event('Model.Product.afterPurchase');
+        $meta = [
+            'communityId' => 1,
+            'productId' => ProductsTable::OFFICIALS_SUMMIT
+        ];
+        $listener->sendDeliverOptPresentationEmail($event, $meta);
+        $this->assertAdminTaskEmailEnqueued('deliverOptionalPresentation');
     }
 }
