7b530b1ae826e7e5878180bf4aee06398d060786
Graham Watson <graham@phantomwatson.com>
2018-01-25 20:01:19 -0500

    Add QueueAdminAlertEmailTask
    This will replace QueueAdminTaskEmailTask, works with the new AdminAlertMailer, and uses the 'alert' variable key instead of 'mailerMethod' for the name of the alert.

---
+++ src/Shell/Task/QueueAdminAlertEmailTask.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Shell\Task;
+
+use Cake\Mailer\MailerAwareTrait;
+use Cake\Network\Exception\InternalErrorException;
+use Queue\Shell\Task\QueueTask;
+
+class QueueAdminAlertEmailTask extends QueueTask
+{
+    use MailerAwareTrait;
+
+    /**
+     * Outputs a message explaining that this task cannot be added via CLI
+     *
+     * @return void
+     */
+    public function add()
+    {
+        $this->err('Task cannot be added via console');
+    }
+
+    /**
+     * Run function.
+     * This function is executed, when a worker is executing a task.
+     * The return parameter will determine, if the task will be marked completed, or be requeued.
+     *
+     * @param array $data The array passed to QueuedTask->createJob()
+     * @param int $id The id of the QueuedTask
+     * @return bool Success
+     */
+    public function run(array $data, $id)
+    {
+        if (!isset($data['alert'])) {
+            throw new InternalErrorException('Mailer method not specified');
+        }
+        try {
+            $this->getMailer('AdminAlert')->send($data['alert'], [$data]);
+        } catch (\Exception $e) {
+            return false;
+        }
+
+        return true;
+    }
+}
