d5fd82670a8836380851b2405c7c5f055579d3e7
Graham Watson <graham@phantomwatson.com>
2018-02-05 21:12:41 -0500

    Remove invalid tests
    Tests that should have replacements but don't yet

--- tests/TestCase/Event/EmailListenerTest.php
+++ tests/TestCase/Event/EmailListenerTest.php
@@ -2,8 +2,6 @@
 namespace App\Test\TestCase\Event;
 
 use App\Event\EmailListener;
-use App\Model\Table\DeliverablesTable;
-use App\Model\Table\ProductsTable;
 use App\Test\TestCase\ApplicationTest;
 use Cake\Event\Event;
 use Cake\ORM\TableRegistry;
@@ -31,39 +29,6 @@ class EmailListenerTest extends ApplicationTest
     }
 
     /**
-     * Tests EmailListener::sendDeliverMandatoryPresentationEmail() sends deliverMandatoryPresentation email
-     *
-     * @return void
-     * @throws \Exception
-     */
-    public function testSendMandPresEmail()
-    {
-        $listener = new EmailListener();
-        $event = new Event('Model.Survey.afterDeactivate');
-        $meta = ['communityId' => 1];
-        $listener->sendDeliverMandatoryPresentationEmail($event, $meta);
-        $this->assertAdminTaskEmailEnqueued('deliverMandatoryPresentation');
-    }
-
-    /**
-     * Tests EmailListener::sendDeliverOptPresentationEmail() sends deliverOptionalPresentation email
-     *
-     * @return void
-     * @throws \Exception
-     */
-    public function testSendOptPresEmail()
-    {
-        $listener = new EmailListener();
-        $event = new Event('Model.Product.afterPurchase');
-        $meta = [
-            'communityId' => 1,
-            'productId' => ProductsTable::OFFICIALS_SUMMIT
-        ];
-        $listener->sendDeliverOptPresentationEmail($event, $meta);
-        $this->assertAdminTaskEmailEnqueued('deliverOptionalPresentation');
-    }
-
-    /**
      * Tests that EmailListener::implementedEvents() contains all required triggers
      *
      * @return void
@@ -72,11 +37,7 @@ class EmailListenerTest extends ApplicationTest
     {
         $required = [
             'Model.Community.afterAutomaticAdvancement' => 'sendCommunityPromotedEmail',
-            'Model.Community.afterScoreIncrease' => 'sendCommunityPromotedEmail',
-            'Model.Survey.afterDeactivate' => 'sendDeliverMandatoryPresentationEmail',
-            'Model.Product.afterPurchase' => 'sendDeliverOptPresentationEmail',
-            'Model.Purchase.afterAdminAdd' => 'sendDeliverOptPresentationEmail',
-            'Model.Delivery.afterAdd' => 'sendSchedulePresentationEmail'
+            'Model.Community.afterScoreIncrease' => 'sendCommunityPromotedEmail'
         ];
         $listener = new EmailListener();
         $actual = $listener->implementedEvents();
@@ -84,73 +45,5 @@ class EmailListenerTest extends ApplicationTest
             $this->assertArrayHasKey($event, $actual);
             $this->assertEquals($method, $actual[$event]);
         }
-    }
-
-    /**
-     * Tests that EmailListener::sendCommunityPromotedEmail() sends createSurvey email
-     *
-     * @return void
-     * @throws \Exception
-     */
-    public function testSendCreateSurveyEmail()
-    {
-        // Create necessary condition
-        $surveysTable = TableRegistry::get('Surveys');
-        $communityId = 1;
-        $surveys = $surveysTable->find()
-            ->where([
-                'community_id' => $communityId
-            ]);
-        foreach ($surveys as $survey) {
-            $surveysTable->delete($survey);
-        }
-
-        // Test if email was enqueued
-        $listener = new EmailListener();
-        $event = new Event('Model.Community.afterScoreIncrease');
-        $meta = [
-            'communityId' => $communityId,
-            'toStep' => 2
-        ];
-        $listener->sendCommunityPromotedEmail($event, $meta);
-        $this->assertAdminTaskEmailEnqueued('createSurvey');
-    }
-
-    /**
-     * Tests EmailListener::sendSchedulePresentationEmail() sends schedulePresentation email
-     *
-     * @return void
-     * @throws \Exception
-     */
-    public function testSendSchedulePresentationEmail()
-    {
-        // Test if email was enqueued
-        $listener = new EmailListener();
-        $event = new Event('Model.Delivery.afterAdd');
-        $meta = [
-            'communityId' => 1,
-            'deliverableId' => DeliverablesTable::PRESENTATION_A_MATERIALS
-        ];
-        $listener->sendSchedulePresentationEmail($event, $meta);
-        $this->assertAdminTaskEmailEnqueued('schedulePresentation');
-    }
-
-    /**
-     * Tests EmailListener::sendCommunityPromotedEmail() sends deliverPolicyDev email
-     *
-     * @return void
-     * @throws \Exception
-     */
-    public function testSendDeliverPolicyDevEmail()
-    {
-        // Test if email was enqueued
-        $listener = new EmailListener();
-        $event = new Event('Model.Community.afterScoreIncrease');
-        $meta = [
-            'communityId' => 1,
-            'toStep' => 4
-        ];
-        $listener->sendCommunityPromotedEmail($event, $meta);
-        $this->assertAdminTaskEmailEnqueued('deliverPolicyDev');
     }
 }
