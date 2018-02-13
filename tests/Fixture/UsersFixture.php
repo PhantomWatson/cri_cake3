6b3fc668e4d24c20166446775604de5b07499629
Graham Watson <graham@phantomwatson.com>
2018-02-12 19:23:24 -0500

    Refactor how user fixture data is set

--- tests/Fixture/UsersFixture.php
+++ tests/Fixture/UsersFixture.php
@@ -50,41 +50,36 @@ class UsersFixture extends TestFixture
         [
             'id' => 1,
             'role' => 'admin',
-            'salutation' => '',
             'name' => 'Test Admin',
             'email' => 'testadmin@example.com',
-            'phone' => '765-285-3399',
-            'title' => 'Test',
-            'organization' => 'Test',
-            'password' => '$2y$10$oaedH1cbAvt/wayrRJlrVeMWtoQSzgBee81iivOmw4tjMlnvfdP/a',
             'all_communities' => true,
             'ici_email_optin' => true,
-            'cber_email_optin' => true,
-            'created' => '2013-10-16 11:49:38',
-            'modified' => '2016-01-26 01:06:26'
+            'cber_email_optin' => true
         ],
         [
             'id' => 2,
             'role' => 'client',
-            'salutation' => '',
             'name' => 'Test Client',
-            'email' => 'testclient1@example.com',
-            'phone' => '765-285-3399',
-            'title' => 'Test',
-            'organization' => 'Test',
-            'password' => '$2y$10$oaedH1cbAvt/wayrRJlrVeMWtoQSzgBee81iivOmw4tjMlnvfdP/a',
-            'all_communities' => false,
-            'ici_email_optin' => false,
-            'cber_email_optin' => false,
-            'created' => '2013-10-16 11:49:38',
-            'modified' => '2016-01-26 01:06:26'
+            'email' => 'testclient1@example.com'
         ],
         [
             'id' => 3,
             'role' => 'client',
-            'salutation' => '',
             'name' => 'Test Client (inactive community)',
-            'email' => 'testclient2@example.com',
+            'email' => 'testclient2@example.com'
+        ]
+    ];
+
+    /**
+     * Initialization function
+     *
+     * @return void
+     */
+    public function init()
+    {
+        parent::init();
+        $defaultData = [
+            'salutation' => '',
             'phone' => '765-285-3399',
             'title' => 'Test',
             'organization' => 'Test',
@@ -94,6 +89,10 @@ class UsersFixture extends TestFixture
             'cber_email_optin' => false,
             'created' => '2013-10-16 11:49:38',
             'modified' => '2016-01-26 01:06:26'
-        ]
-    ];
+        ];
+
+        foreach ($this->records as &$record) {
+            $record += $defaultData;
+        }
+    }
 }
