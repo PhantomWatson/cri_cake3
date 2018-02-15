db52845eaffad3c8854fccfcddde217c152f81fd
Graham Watson <graham@phantomwatson.com>
2018-02-15 18:00:50 -0500

    Exclude dummy communities from map

--- src/Maps/Map.php
+++ src/Maps/Map.php
@@ -264,7 +264,7 @@ class Map
     {
         $communitiesTable = TableRegistry::get('Communities');
         $communities = $communitiesTable->find()
-            ->where()
+            ->where(['dummy' => false])
             ->orderAsc('name')
             ->all();
 
