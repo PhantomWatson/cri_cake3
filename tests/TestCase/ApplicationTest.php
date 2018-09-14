a1ac9025ac541a550c81e9ed583db1729bf7430c
Graham Watson <graham@phantomwatson.com>
2018-09-14 15:55:12 -0400

    Update with code from CakePHP skeleton app

--- tests/TestCase/ApplicationTest.php
+++ tests/TestCase/ApplicationTest.php
@@ -1,16 +1,16 @@
 <?php
 /**
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
  *
  * Licensed under The MIT License
  * For full copyright and license information, please see the LICENSE.txt
  * Redistributions of files must retain the above copyright notice
  *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
  * @since         3.3.0
- * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
  */
 namespace App\Test\TestCase;
 
@@ -20,21 +20,19 @@ use Cake\Database\Expression\QueryExpression;
 use Cake\Error\Middleware\ErrorHandlerMiddleware;
 use Cake\Event\EventList;
 use Cake\Http\MiddlewareQueue;
+use Cake\Http\Middleware\CsrfProtectionMiddleware;
 use Cake\ORM\TableRegistry;
 use Cake\Routing\Middleware\AssetMiddleware;
 use Cake\Routing\Middleware\RoutingMiddleware;
 use Cake\Routing\Router;
 use Cake\TestSuite\IntegrationTestCase;
+use InvalidArgumentException;
 
 /**
  * ApplicationTest class
  */
 class ApplicationTest extends IntegrationTestCase
 {
-
-    public $adminUser;
-    public $clientUser;
-
     /**
      * Sets up this set of tests
      *
@@ -57,6 +55,43 @@ class ApplicationTest extends IntegrationTestCase
     }
 
     /**
+     * testBootstrap
+     *
+     * @return void
+     */
+    public function testBootstrap()
+    {
+        $app = new Application(dirname(dirname(__DIR__)) . '/config');
+        $app->bootstrap();
+        $plugins = $app->getPlugins();
+
+        $this->assertCount(3, $plugins);
+        $this->assertSame('Bake', $plugins->get('Bake')->getName());
+        $this->assertSame('Migrations', $plugins->get('Migrations')->getName());
+        $this->assertSame('DebugKit', $plugins->get('DebugKit')->getName());
+    }
+
+    /**
+     * testBootstrapPluginWitoutHalt
+     *
+     * @return void
+     */
+    public function testBootstrapPluginWitoutHalt()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        $app = $this->getMockBuilder(Application::class)
+            ->setConstructorArgs([dirname(dirname(__DIR__)) . '/config'])
+            ->setMethods(['addPlugin'])
+            ->getMock();
+
+        $app->method('addPlugin')
+            ->will($this->throwException(new InvalidArgumentException('test exception.')));
+
+        $app->bootstrap();
+    }
+
+    /**
      * testMiddleware
      *
      * @return void
@@ -71,6 +106,7 @@ class ApplicationTest extends IntegrationTestCase
         $this->assertInstanceOf(ErrorHandlerMiddleware::class, $middleware->get(0));
         $this->assertInstanceOf(AssetMiddleware::class, $middleware->get(1));
         $this->assertInstanceOf(RoutingMiddleware::class, $middleware->get(2));
+        $this->assertInstanceOf(CsrfProtectionMiddleware::class, $middleware->get(3));
     }
 
     /**
