5fddff668875a50e6f9e6582a051bf06840c7caf
Graham Watson <graham@phantomwatson.com>
2018-09-14 12:04:10 -0400

    Upgrade Application class with CakePHP skeleton app code

--- src/Application.php
+++ src/Application.php
@@ -1,22 +1,24 @@
 <?php
 /**
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
  *
  * Licensed under The MIT License
  * For full copyright and license information, please see the LICENSE.txt
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link      http://cakephp.org CakePHP(tm) Project
+ * @copyright Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link      https://cakephp.org CakePHP(tm) Project
  * @since     3.3.0
- * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ * @license   https://opensource.org/licenses/mit-license.php MIT License
  */
 namespace App;
 
 use Cake\Core\Configure;
+use Cake\Core\Exception\MissingPluginException;
 use Cake\Error\Middleware\ErrorHandlerMiddleware;
 use Cake\Http\BaseApplication;
+use Cake\Http\Middleware\CsrfProtectionMiddleware;
 use Cake\Routing\Middleware\AssetMiddleware;
 use Cake\Routing\Middleware\RoutingMiddleware;
 
@@ -29,12 +31,11 @@ use Cake\Routing\Middleware\RoutingMiddleware;
 class Application extends BaseApplication
 {
     /**
-     * Application bootstrap method
-     *
-     * @return void
+     * {@inheritDoc}
      */
     public function bootstrap()
     {
+        // Call parent to load bootstrap from files.
         parent::bootstrap();
 
         $this->addPlugin('Muffin/Slug');
@@ -45,30 +46,54 @@ class Application extends BaseApplication
         $this->addPlugin('GoogleCharts');
         $this->addPlugin('Xety/Cake3CookieAuth');
 
+        if (PHP_SAPI === 'cli') {
+            try {
+                $this->addPlugin('Bake');
+            } catch (MissingPluginException $e) {
+                // Do not halt if the plugin is missing
+            }
+
+            $this->addPlugin('Migrations');
+        }
+
+        /*
+         * Only try to load DebugKit in development mode
+         * Debug Kit should not be installed on a production system
+         */
         if (Configure::read('debug')) {
-            $this->addPlugin('DebugKit');
+            $this->addPlugin(\DebugKit\Plugin::class);
         }
     }
 
     /**
-     * Setup the middleware your application will use.
+     * Setup the middleware queue your application will use.
      *
-     * @param \Cake\Http\MiddlewareQueue $middleware The middleware queue to setup.
-     * @return \Cake\Http\MiddlewareQueue The updated middleware.
+     * @param \Cake\Http\MiddlewareQueue $middlewareQueue The middleware queue to setup.
+     * @return \Cake\Http\MiddlewareQueue The updated middleware queue.
      */
-    public function middleware($middleware)
+    public function middleware($middlewareQueue)
     {
-        $middleware
+        $middlewareQueue
             // Catch any exceptions in the lower layers,
             // and make an error page/response
             ->add(ErrorHandlerMiddleware::class)
 
             // Handle plugin/theme assets like CakePHP normally does.
-            ->add(AssetMiddleware::class)
+            ->add(new AssetMiddleware([
+                'cacheTime' => Configure::read('Asset.cacheTime')
+            ]))
 
-            // Apply routing
-            ->add(new RoutingMiddleware($this));
+            // Add routing middleware.
+            // Routes collection cache enabled by default, to disable route caching
+            // pass null as cacheConfig, example: `new RoutingMiddleware($this)`
+            // you might want to disable this cache in case your routing is extremely simple
+            ->add(new RoutingMiddleware($this, '_cake_routes_'))
 
-        return $middleware;
+            // Add csrf middleware.
+            ->add(new CsrfProtectionMiddleware([
+                'httpOnly' => true
+            ]));
+
+        return $middlewareQueue;
     }
 }
