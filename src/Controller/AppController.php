f8b97d1fd40664594e5afe137cdc63d67fb03ddb
Graham Watson <graham@phantomwatson.com>
2020-02-12 16:48:55 -0500

    Fix deprecated request type code

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -195,7 +195,7 @@ class AppController extends Controller
         }
 
         if (!array_key_exists('_serialize', $this->viewVars) &&
-            in_array($this->response->type(), ['application/json', 'application/xml'])
+            in_array($this->response->getType(), ['application/json', 'application/xml'])
         ) {
             $this->set('_serialize', true);
         }
